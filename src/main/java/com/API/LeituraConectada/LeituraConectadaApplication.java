c1ef0c8fa31d8fe163fa9fd98e41cbbbe88b3d1d
igormerten <igormeerten@gmail.com>
2026-05-27 20:00:22 -0300

    Primeiro commit

---
+++ src/main/java/com/API/LeituraConectada/LeituraConectadaApplication.java
@@ -0,0 +1,13 @@
+package com.API.LeituraConectada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeituraConectadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeituraConectadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/API/LeituraConectada/LeituraConectadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.API.LeituraConectada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeituraConectadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
