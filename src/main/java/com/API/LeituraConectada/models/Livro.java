103a34353cfce0ec538e08b87f211157519cea6e
joaolarocha-dotcom <joao.larocha@souunit.com.br>
2026-06-01 15:58:26 -0300

    Add files via upload

--- src/main/java/com/API/LeituraConectada/models/Livro.java
+++ src/main/java/com/API/LeituraConectada/models/Livro.java
@@ -1,27 +1,32 @@
 package com.API.LeituraConectada.models;
 
-import lombok.*;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.Set;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
-
+@AllArgsConstructor
+@Entity
 public class Livro {
-    private int id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
     private String titulo;
     private String autor;
     private int quantidadeDisponivel;
 
-    public Livro(String titulo, String autor, int quantidadeDisponivel) {
-        this.titulo = titulo;
-        this.autor = autor;
-        this.quantidadeDisponivel = quantidadeDisponivel;
-    };
+    @ElementCollection(fetch = FetchType.EAGER) // Cria a tabela relacional automaticamente
+    @CollectionTable(name = "livro_categorias", joinColumns = @JoinColumn(name = "livro_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Categoria> categorias; // Guarda a lista de enums fixos
 
     public boolean verificaDisponibilidade(){
         return this.quantidadeDisponivel > 0;
     }
-
-
 }
--- src/main/java/com/API/LeituraConectada/models/Usuario.java
+++ src/main/java/com/API/LeituraConectada/models/Usuario.java
@@ -1,4 +1,4 @@
-package com.API.LeituraConectada.model;
+package com.API.LeituraConectada.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
